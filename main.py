5cb73121b1dd2542e735fff2b2d86ce751a3417c
Gregory-Davies <Ross.Gregory-Davies@ons.gov.uk>
2021-10-28 12:06:20 +0100

    Pages first case working

--- main.py
+++ main.py
@@ -1,30 +1,56 @@
 import sys
-from PyQt5.uic import loadUi, loadUiType
-from PyQt5.QtWidgets import QApplication, QGridLayout, QMainWindow, QWidget
+from PyQt5.uic import loadUi
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from dataclasses import dataclass
 
-@dataclass(frozen=True)
-class Page(QWidget):
-    """"""
-    id: int
-    ui_path: str
+@dataclass
+class Pages:
+    list = []
+    
+    def add_page(self, ui_path):
+        widg = loadUi(ui_path)
+        self.list.append(widg)
+
+    def __init__(self):
+        self.add_page("load.ui")
+        self.add_page("options.ui")
+        self.add_page("execute.ui")
+
+    def next(self, current: QWidget):
+        return self.list[self.list.index(current)+1]
+
+    def prev(self, current: QWidget):
+        return self.list[self.list.index(current)-1]
+
+    def first(self):
+        return self.list[0]
+
+    def last(self):
+        return self.list[-1]
+
 
 class MainWindow(QMainWindow, object):
-    def __init__(self, *args: QWidget):
+    def __init__(self):
         super(MainWindow, self).__init__()
         loadUi("main.ui", self)
+        self.pages = Pages()
+        self.setCentralWidget(self.pages.list[0])
+        self.centralWidget().commandLinkButton_next.clicked.connect(lambda: self.next_page())
     
-    def set_active_page(self, page: Page):
-        self.setCentralWidget(loadUi(page.ui_path))
+    def set_active_page(self, page: QWidget):
+        self.setCentralWidget(page)
+
+    def next_page(self):
+        self.setCentralWidget(self.pages.next(self.centralWidget()))
+
+    def prev_page(self):
+        self.setCentralWidget(self.pages.prev(self.centralWidget()))
 
 
 def main():
     app = QApplication(sys.argv)
     mainwindow = MainWindow()
-    load_page = Page(1, "load.ui")
-    print(load_page.id)
     mainwindow.show()
-    mainwindow.set_active_page(load_page)
     try:
         sys.exit(app.exec_())
     except:
