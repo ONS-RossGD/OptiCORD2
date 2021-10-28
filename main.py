5195b4211359eb1d176ee0b5ed63b54dec363212
Gregory-Davies <Ross.Gregory-Davies@ons.gov.uk>
2021-10-28 15:02:02 +0100

    Themes working

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import sys
+import BreezeStyleSheets.breeze_resources # looks redundant but is used to activate stylesheets
+from PyQt5.QtCore import QFile, QTextStream
 from PyQt5.uic import loadUi
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
-from dataclasses import dataclass
+from dataclasses import dataclass, field
+from typing import List
 
 @dataclass
 class Pages:
@@ -28,15 +31,40 @@ class Pages:
     def last(self):
         return self.list[-1]
 
+@dataclass
+class Theme:
+    theme: str = field(init=False)
+    theme_name: str
+    app: QApplication
+
+    def __post_init__(self):
+        file = QFile(":/"+self.theme_name+"/stylesheet.qss")
+        file.open(QFile.ReadOnly | QFile.Text)
+        stream = QTextStream(file)
+        self.theme = stream.readAll()
+    
+    def activate(self):
+        self.app.setStyleSheet(self.theme)
 
 class MainWindow(QMainWindow, object):
-    def __init__(self):
+    def __init__(self, themes: dict):
         super(MainWindow, self).__init__()
         loadUi("main.ui", self)
         self.pages = Pages()
+        self.themes = themes
+        self.themes["dark-purple"].activate()
         self.setCentralWidget(self.pages.list[0])
-        self.centralWidget().commandLinkButton_next.clicked.connect(lambda: self.next_page())
+        self.centralWidget().commandLinkButton_next.clicked.connect(lambda: self.themes["dark-purple"].activate())
+        self.setup_singals()
     
+    def setup_singals(self):
+        self.dark_blue_theme.triggered.connect(lambda: self.themes["dark-blue"].activate())
+        self.dark_green_theme.triggered.connect(lambda: self.themes["dark-green"].activate())
+        self.dark_purple_theme.triggered.connect(lambda: self.themes["dark-purple"].activate())
+        self.light_blue_theme.triggered.connect(lambda: self.themes["light-blue"].activate())
+        self.light_green_theme.triggered.connect(lambda: self.themes["light-green"].activate())
+        self.light_purple_theme.triggered.connect(lambda: self.themes["light-purple"].activate())
+
     def set_active_page(self, page: QWidget):
         self.setCentralWidget(page)
 
@@ -47,9 +75,20 @@ class MainWindow(QMainWindow, object):
         self.setCentralWidget(self.pages.prev(self.centralWidget()))
 
 
+def setup_themes(app: QApplication):
+    themes = {}
+    themes["dark-blue"] = Theme("dark", app)
+    themes["dark-green"] = Theme("dark-green", app)
+    themes["dark-purple"] = Theme("dark-purple", app)
+    themes["light-blue"] = Theme("light", app)
+    themes["light-green"] = Theme("light-green", app)
+    themes["light-purple"] = Theme("light-purple", app)
+    return themes
+
 def main():
     app = QApplication(sys.argv)
-    mainwindow = MainWindow()
+    themes = setup_themes(app)
+    mainwindow = MainWindow(themes)
     mainwindow.show()
     try:
         sys.exit(app.exec_())
